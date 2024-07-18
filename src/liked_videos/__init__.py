bc51fcff76eed367a73e7d84a29dfb0abfdfd74f
Michael Uloth <hello@michaeluloth.com>
2024-07-18 10:33:09 -0400

    docs: add TODOs

--- src/liked_videos/__init__.py
+++ src/liked_videos/__init__.py
@@ -2,18 +2,30 @@ import logging
 from typing import Literal
 
 from liked_videos import log
+
+# from liked_videos import notes
 from liked_videos import youtube
 
 
 log.setup_logging()  # one-time setup on startup
+
 logger = logging.getLogger(__name__)
 
 
+# TODO: run tests, linting, type checking, tests as a pre-commit hook (since committing to main)?
+# TODO: persist logs somewhere? file? sqlite db? cloud logging service?
 def main() -> Literal[0, 1]:
     try:
         client = youtube.create_authenticated_client()
-        liked_videos = youtube.fetch_liked_videos(client)
+        liked_youtube_videos = youtube.fetch_liked_videos(client)
+        # TODO: fetch youtube video IDs included in my notes (reference local path to notes? assume host has no such path and temporarily clone the repo?)
+        # youtube_videos_in_notes = notes.fetch_youtube_video_ids()
+        # TODO: identify which liked videos are missing from my notes
+        # TODO: identify which videos in my notes aren't like on YouTube? Do I want to be aware of those so I can go like them?
+        # TODO: identify which videos in my notes are no longer available on YouTube? Do I want to be aware of those so I can remove them from my notes?
+        # TODO: generate markdown links for each list of videos I want to add to my notes
+        # TODO: append the markdown links to my inbox note (or a bots-only inbox? do I want a heading for each list of videos? with the date?)
         return 0
     except Exception as e:
-        print(repr(e))
+        print("Unexpected error:\n", repr(e))
         return 1
--- src/liked_videos/log.py
+++ src/liked_videos/log.py
@@ -1,6 +1,7 @@
 import logging
 
 
+# TODO: add color? by log level? by log message section?
 def setup_logging() -> None:
     logging.basicConfig(
         level=logging.INFO,
